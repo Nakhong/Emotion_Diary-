2f5e75ff01219532329125f0a3f0755e6f7b181c
HwanakHong <ghdghkwo5@naver.com>
2022-04-08 20:23:24 +0900

    first commit

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,24 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+
+  const mode = searchParams.get("mode");
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 일기 수정 페이지 입니다.</p>
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        HOme
+      </button>
     </div>
   );
 };
